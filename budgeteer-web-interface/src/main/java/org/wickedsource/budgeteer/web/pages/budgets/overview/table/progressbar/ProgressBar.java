9692076f16153b5136b0a387578b8c15c5837d30
Tom Hombergs <tom.hombergs@gmail.com>
2014-10-22 22:10:11 +0200

    added missing files

---
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/budgets/overview/table/progressbar/ProgressBar.java
@@ -0,0 +1,50 @@
+package org.wickedsource.budgeteer.web.pages.budgets.overview.table.progressbar;
+
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.protocol.http.WebSession;
+
+public class ProgressBar extends Panel {
+
+    public ProgressBar(String id, IModel<Double> percentModel) {
+        super(id, percentModel);
+        add(createBar("bar"));
+    }
+
+    private Label createBar(String id) {
+        Label bar = new Label(id, new AbstractReadOnlyModel<String>() {
+            @Override
+            public String getObject() {
+                return String.format(WebSession.get().getLocale(),"%.2f%%", getModel().getObject());
+            }
+        });
+        bar.add(new AttributeAppender("class", new AbstractReadOnlyModel<String>() {
+            @Override
+            public String getObject() {
+                Double progress = getModel().getObject();
+                if (progress > 90d) {
+                    return " progress-bar-danger";
+                } else if (progress > 75d) {
+                    return " progress-bar-warning";
+                } else {
+                    return " progress-bar-success";
+                }
+            }
+        }));
+        bar.add(new AttributeAppender("style", new AbstractReadOnlyModel<String>() {
+            @Override
+            public String getObject() {
+                return String.format("width: %d%%", getModel().getObject().intValue());
+            }
+        }));
+        return bar;
+    }
+
+    @SuppressWarnings("unchecked")
+    private IModel<Double> getModel() {
+        return (IModel<Double>) getDefaultModel();
+    }
+}
