d183d91c6f9e6a1d08e2e725e2d0b931ac6f0796
Chris <ch-eh@gmx.de>
2014-12-10 15:05:54 +0100

    Trying to fix the wrong classname

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/service/user/UsernameAlreadyInuseException.java
+++
@@ -1,7 +0,0 @@
-package org.wickedsource.budgeteer.service.user;
-
-/**
- * Exception is thrown during the registration process, if the username is already in-use
- */
-public class UsernameAlreadyInUseException extends  Exception {
-}
