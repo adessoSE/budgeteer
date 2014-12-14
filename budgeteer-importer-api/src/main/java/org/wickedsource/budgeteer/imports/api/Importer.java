fca3f00aff9ec45a11362dc5e4457d17818b8655
Tom Hombergs <tom.hombergs@gmail.com>
2014-12-14 21:47:03 +0100

    javadoc / comments

--- budgeteer-importer-api/src/main/java/org/wickedsource/budgeteer/imports/api/Importer.java
+++ budgeteer-importer-api/src/main/java/org/wickedsource/budgeteer/imports/api/Importer.java
@@ -5,10 +5,26 @@ import java.util.List;
 
 public interface Importer extends Serializable {
 
+    /**
+     * Returns the name of this importer as it should be displayed in the UI.
+     *
+     * @return the human readable name of this importer.
+     */
     String getDisplayName();
 
+    /**
+     * Returns the file extensions of import file that this importer understands.
+     *
+     * @return list of file extensions (with leading ".").
+     */
     List<String> getSupportedFileExtensions();
 
+    /**
+     * Returns an example file that contains data in the format that this importer understands.
+     * This file can be downloaded by the user to learn the import format.
+     *
+     * @return example file with the data format that this importer understands.
+     */
     ExampleFile getExampleFile();
 
 }
