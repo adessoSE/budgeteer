4056aab6018df8af56c6454774ef1d47ade43e1c
ehmke <ch-eh@gmx.de>
2015-01-27 12:06:43 +0100

    Changes the multiselect-behavior a little bit so it is easier to find the correct entry (e.g. in the edit person page)

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/components/multiselect/MultiselectBehavior.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/components/multiselect/MultiselectBehavior.java
@@ -2,11 +2,10 @@ package org.wickedsource.budgeteer.web.components.multiselect;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
-import org.apache.wicket.markup.ComponentTag;
-import org.apache.wicket.markup.head.*;
-import org.apache.wicket.markup.head.filter.FilteredHeaderItem;
-import org.apache.wicket.markup.head.filter.JavaScriptFilteredIntoFooterHeaderResponse;
-import org.apache.wicket.request.Response;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.wickedsource.budgeteer.web.BudgeteerReferences;
 
 import java.util.HashMap;
@@ -51,5 +50,23 @@ public class MultiselectBehavior extends Behavior{
         }
         script.append(");");
        return script.toString();
+    }
+
+    /**
+     * Returns a HashMap with the budgeteer-typical-options set.
+     * The set options are: includeSelectAllOption, buttonWidth, maxHeight, numberDisplayed, buttonClass,
+     * enableFiltering, maxHeight
+     * @return HashMap with the mentioned options set to typical values
+     */
+    public static HashMap<String, String> getRecommendedOptions(){
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("includeSelectAllOption","true");
+        options.put("buttonWidth","'220px'");
+        options.put("maxHeight","250");
+        options.put("numberDisplayed","2");
+        options.put("buttonClass","'btn btn-default btn-sm'");
+        options.put("enableFiltering","true");
+        options.put("enableCaseInsensitiveFiltering", "true");
+        return options;
     }
 }
