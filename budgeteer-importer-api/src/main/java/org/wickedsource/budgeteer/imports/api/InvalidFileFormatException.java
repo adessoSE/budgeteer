03896c1283ef558dcac3fc9b2cdd7cd0854335d9
ehmke <ch-eh@gmx.de>
2015-08-31 12:24:05 +0200

    Issue #53 - check for valid file format

---
+++ budgeteer-importer-api/src/main/java/org/wickedsource/budgeteer/imports/api/InvalidFileFormatException.java
@@ -0,0 +1,7 @@
+package org.wickedsource.budgeteer.imports.api;
+
+public class InvalidFileFormatException extends Exception {
+    public InvalidFileFormatException(String message){
+        super(message);
+    }
+}
