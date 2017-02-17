603fba8cb55c20385ff583082f5c758e0e0f53b7
David Klassen <daviddd.kl@gmail.com>
2017-02-17 10:53:50 +0100

    made contract and description fields nullable in the view

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/budgets/details/highlights/BudgetHighlightsPanel.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/budgets/details/highlights/BudgetHighlightsPanel.java
@@ -6,7 +6,10 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.wickedsource.budgeteer.service.budget.BudgetDetailData;
 import org.wickedsource.budgeteer.web.components.money.MoneyLabel;
+import org.wickedsource.budgeteer.web.components.nullmodel.NullsafeModel;
 import org.wickedsource.budgeteer.web.components.percent.PercentageLabel;
+
+import java.sql.Date;
 
 import static org.wicketstuff.lazymodel.LazyModel.from;
 import static org.wicketstuff.lazymodel.LazyModel.model;
@@ -16,20 +19,17 @@ public class BudgetHighlightsPanel extends Panel {
     public BudgetHighlightsPanel(String id, IModel<BudgetDetailData> model) {
         super(id, model);
         add(new Label("name", model(from(model).getName())));
-        add(new Label("contract", new PropertyModel<String>(model, "contractName"){
-            @Override
-            public String getObject() {
-                String contract = super.getObject();
-                contract = contract == null ? getString("no.contract") : contract;
-                return contract;
-            }
-        }));
-        add(new Label("description", model(from(model).getDescription())));
+        add(new Label("contract", nullsafeModel(model(from(model).getContractName()))));
+        add(new Label("description", nullsafeModel(model(from(model).getDescription()))));
         add(new MoneyLabel("total", model(from(model).getTotal()), true));
         add(new MoneyLabel("remaining", model(from(model).getRemaining()), true));
         add(new PercentageLabel("progress", model(from(model).getProgress())));
         add(new MoneyLabel("avgDailyRate", model(from(model).getAvgDailyRate()), true));
         add(new Label("lastUpdated", model(from(model).getLastUpdated())));
+    }
+
+    private IModel<String> nullsafeModel(IModel<String> wrappedModel){
+        return new NullsafeModel<>(wrappedModel, getString("nullString"));
     }
 
 }
