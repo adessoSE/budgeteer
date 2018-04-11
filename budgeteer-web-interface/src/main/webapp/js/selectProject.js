4b7350754083e10f4eba928d059f1e1b8b9cde86
Alexander Nedomansky <34765163+anedomansky@users.noreply.github.com>
2018-04-11 12:55:08 +0200

    Added default actions 'enter' and 'esc' to invoice details pages.

--- budgeteer-web-interface/src/main/webapp/js/selectProject.js
+++ budgeteer-web-interface/src/main/webapp/js/selectProject.js
@@ -1,50 +1,56 @@
+// wait until the DOM is ready
 window.onload = (function () {
 
     // Get the input field
     var input = document.getElementById("proCh");
     var count = 0;
 
+    // get the selected item from the dropdown list
     if(input.options[input.selectedIndex].value != "") {
         count = 1;
     }
 
+    // add listener for keydown-event
     document.addEventListener("keydown", function(evt) {
+        // get the event
         evt = evt || window.event;
         var isEscape = false;
+        // get the key
         if ("key" in evt) {
             isEscape = (evt.key == "Escape" || evt.key == "Esc");
         } else {
             isEscape = (evt.keyCode == 27);
         }
+        // if esc is pressed, click logout
         if (isEscape) {
             evt.preventDefault();
             document.getElementById("logout").click();
         }
     });
 
-    // Execute a function when the user presses enter on the keyboard
+    // add listener for keypress-event
     input.addEventListener("keypress", function(event) {
       // Number 13 is the "Enter" key on the keyboard
       if (event.keyCode === 13 && count != 1) {
         count = 1;
         console.log("count is" + count);
-        // Trigger the button element with a click
+        // click the dropdown list item
         document.getElementById("proCh").click();
       }
       else if(event.keyCode === 13 && count == 1) {
         event.preventDefault();
         count = 0;
+        // click the submit-button
         document.getElementById("goBtn").click();
       }
 
     });
 
+    // add listener for the click-event
     input.addEventListener("click", function(event) {
       if (count == 0) {
         count += 1;
       }
-
-
     });
 
 });
