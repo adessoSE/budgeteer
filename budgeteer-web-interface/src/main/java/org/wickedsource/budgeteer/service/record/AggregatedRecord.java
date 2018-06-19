125d55e72205f3c6eb5026646444ff22452ed507
adis <Leonie-Camilla.Adis@adesso.de>
2018-06-19 15:58:46 +0200

    Issue #207: adding possibility to change between gross and net values in monthly and weekly overviews of all budgets or one budget

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/service/record/AggregatedRecord.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/service/record/AggregatedRecord.java
@@ -1,24 +1,33 @@
 package org.wickedsource.budgeteer.service.record;
 
 import lombok.Data;
+import org.apache.poi.hpsf.SummaryInformation;
 import org.joda.money.Money;
 import org.wickedsource.budgeteer.MoneyUtil;
 
-import java.util.Date;
+import javax.xml.transform.Result;
+import java.util.*;
 
 @Data
 public class AggregatedRecord {
-
     private String aggregationPeriodTitle;
     private Date aggregationPeriodStart;
     private Date aggregationPeriodEnd;
     private Double hours;
     private Money budgetPlanned;
     private Money budgetBurned;
+    private Money budgetPlanned_gross;
+    private Money budgetBurned_gross;
 
     public Money getDifference() {
         Money first = budgetPlanned != null ? budgetPlanned : MoneyUtil.createMoney(0);
         Money second = budgetBurned != null ? budgetBurned : MoneyUtil.createMoney(0);
+        return first.minus(second);
+    }
+
+    public Money getDifference_gross(){
+        Money first = budgetPlanned_gross != null ? budgetPlanned_gross : MoneyUtil.createMoney(0);
+        Money second = budgetBurned_gross != null ? budgetBurned_gross : MoneyUtil.createMoney(0);
         return first.minus(second);
     }
 }
