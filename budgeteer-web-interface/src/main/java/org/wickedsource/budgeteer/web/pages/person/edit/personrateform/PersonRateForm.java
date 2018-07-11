33e5184e27809d49e760c1af43c8d2d0e3e9af2f
maximAtanasov <Maksim.Atanasov@adesso.de>
2018-07-11 12:25:32 +0200

    fixed a bug where one could not select any budget in the dropdown - issue #253

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/person/edit/personrateform/PersonRateForm.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/person/edit/personrateform/PersonRateForm.java
@@ -1,23 +1,28 @@
 package org.wickedsource.budgeteer.web.pages.person.edit.personrateform;
 
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.extensions.markup.html.form.select.IOptionRenderer;
+import org.apache.wicket.extensions.markup.html.form.select.Select;
+import org.apache.wicket.extensions.markup.html.form.select.SelectOptions;
 import org.apache.wicket.injection.Injector;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.ListMultipleChoice;
-import org.apache.wicket.model.util.ListModel;
+import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wickedsource.budgeteer.service.DateRange;
 import org.wickedsource.budgeteer.service.budget.BudgetBaseData;
 import org.wickedsource.budgeteer.service.budget.BudgetService;
 import org.wickedsource.budgeteer.service.person.PersonRate;
 import org.wickedsource.budgeteer.web.BudgeteerSession;
-import org.wickedsource.budgeteer.web.components.budget.BudgetBaseDataChoiceRenderer;
 import org.wickedsource.budgeteer.web.components.daterange.DateRangeInputField;
-import org.wickedsource.budgeteer.web.components.listMultipleChoiceWithGroups.ListMultipleChoiceWithGroups;
 import org.wickedsource.budgeteer.web.components.listMultipleChoiceWithGroups.OptionGroup;
 import org.wickedsource.budgeteer.web.components.money.MoneyTextField;
 import org.wickedsource.budgeteer.web.components.multiselect.MultiselectBehavior;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -27,9 +32,9 @@ import static org.wicketstuff.lazymodel.LazyModel.model;
 public abstract class PersonRateForm extends Form<PersonRateForm.PersonRateFormModel> {
 
     public static class PersonRateFormModel extends PersonRate {
-        private ListModel<BudgetBaseData> chosenBudgets = new ListModel<>();
+        private Model<ArrayList<BudgetBaseData>> chosenBudgets = new Model<>(new ArrayList<>());
 
-        public ListModel<BudgetBaseData> getChosenBudgets() {
+        public Model<ArrayList<BudgetBaseData>> getChosenBudgets() {
             return chosenBudgets;
         }
     }
@@ -51,17 +56,7 @@ public abstract class PersonRateForm extends Form<PersonRateForm.PersonRateFormM
         add(dateRangeField);
 
         List<OptionGroup<BudgetBaseData>> possibleBudgets = budgetService.getPossibleBudgetDataForPersonAndProject(BudgeteerSession.get().getProjectId(), personId);
-        ListMultipleChoice<BudgetBaseData> budgetChoice =
-                new ListMultipleChoiceWithGroups<>("budgetField", getModelObject().getChosenBudgets(), possibleBudgets, new BudgetBaseDataChoiceRenderer());
-
-        budgetChoice.setRequired(true);
-        budgetChoice.setChoiceRenderer(new BudgetBaseDataChoiceRenderer());
-
-        HashMap<String, String> multiselectOptions = MultiselectBehavior.getRecommendedOptions();
-        multiselectOptions.put("includeSelectAllOption","false");
-        budgetChoice.add(new MultiselectBehavior(multiselectOptions));
-
-        add(budgetChoice);
+        addBudgetsDropdown(possibleBudgets);
 
         add(new Button("submitButton"));
     }
@@ -69,19 +64,19 @@ public abstract class PersonRateForm extends Form<PersonRateForm.PersonRateFormM
     @Override
     protected void onSubmit() {
         PersonRateFormModel addedPersonRate = getModelObject();
-        for (BudgetBaseData budget : addedPersonRate.getChosenBudgets().getObject()) {
+        for(BudgetBaseData budget : addedPersonRate.getChosenBudgets().getObject()) {
             List<PersonRate> overlappingRate = getOverlappingRates(addedPersonRate.getDateRange(), budget);
-            if(overlappingRate == null || overlappingRate.isEmpty()) {
+            if (overlappingRate == null || overlappingRate.isEmpty()) {
                 rateAdded(new PersonRate(addedPersonRate.getRate(), budget, addedPersonRate.getDateRange()));
-            }else {
+            } else {
                 StringBuilder overlappingEntryNames = new StringBuilder();
                 overlappingEntryNames.append(System.getProperty("line.separator"));
-                for(int i=0; i < overlappingRate.size(); i++){
+                for (int i = 0; i < overlappingRate.size(); i++) {
                     PersonRate p = overlappingRate.get(i);
                     overlappingEntryNames.append(p.getBudget().getName());
                     overlappingEntryNames.append(" ");
                     overlappingEntryNames.append(p.getDateRange().toString());
-                    if(i < overlappingRate.size() - 1){
+                    if (i < overlappingRate.size() - 1) {
                         overlappingEntryNames.append(",");
                         overlappingEntryNames.append(System.getProperty("line.separator"));
                     }
@@ -102,4 +97,39 @@ public abstract class PersonRateForm extends Form<PersonRateForm.PersonRateFormM
      * @return a List of PersonRates (if there are no PersonRates that end or start in the dateRange, a empty List will be returned)
      */
     protected abstract List<PersonRate> getOverlappingRates(DateRange dateRange, BudgetBaseData budget);
+
+    /**
+     * This methods adds a dropdown menu for the available budgets.
+     * @param possibleBudgets A list of grouped possible budgets
+     */
+    private void addBudgetsDropdown(List<OptionGroup<BudgetBaseData>> possibleBudgets){
+        Select<ArrayList<BudgetBaseData>> select = new Select<>("select", getModelObject().getChosenBudgets());
+        HashMap<String, String> multiselectOptions = MultiselectBehavior.getRecommendedOptions();
+        multiselectOptions.put("includeSelectAllOption","false");
+        add(select.setRequired(true).add(new MultiselectBehavior(multiselectOptions)));
+        RepeatingView rv = new RepeatingView("repeatingView");
+        rv.setOutputMarkupId(true);
+        rv.setOutputMarkupPlaceholderTag(true);
+        select.add(rv);
+        for(OptionGroup<BudgetBaseData> group : possibleBudgets){
+            WebMarkupContainer overOptGroup = new WebMarkupContainer(rv.newChildId());
+            rv.add(overOptGroup);
+            WebMarkupContainer optGroup = new WebMarkupContainer("optGroup");
+            overOptGroup.add(optGroup);
+            optGroup.add(new AttributeModifier("label", new Model<>(group.getGroupNameResourceKey())));
+            Model<ArrayList<BudgetBaseData>> a = new Model<>(new ArrayList<>(group.getOptions()));
+            optGroup.add(
+                    new SelectOptions<>("selectOptions", a, new IOptionRenderer<BudgetBaseData>() {
+                        @Override
+                        public String getDisplayValue(BudgetBaseData object) {
+                            return object.getName();
+                        }
+                        @Override
+                        public IModel<BudgetBaseData> getModel(BudgetBaseData value) {
+                            return new Model<>(value);
+                        }
+                    })
+            );
+        }
+    }
 }
