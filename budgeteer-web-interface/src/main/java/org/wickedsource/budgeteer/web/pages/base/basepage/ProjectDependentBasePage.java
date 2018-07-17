119da67069b740cccd11aeac1f04ea78af96f001
Daniel Meier <Daniel.Meier@adesso.de>
2018-07-17 14:26:26 +0200

    added new abstract base page class for webpages that depend on the existence of a selected project

---
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/base/basepage/ProjectDependentBasePage.java
@@ -0,0 +1,8 @@
+package org.wickedsource.budgeteer.web.pages.base.basepage;
+
+import org.wickedsource.budgeteer.web.components.initialization.NeedsProject;
+
+@NeedsProject
+public abstract class ProjectDependentBasePage extends BasePage {
+
+}
