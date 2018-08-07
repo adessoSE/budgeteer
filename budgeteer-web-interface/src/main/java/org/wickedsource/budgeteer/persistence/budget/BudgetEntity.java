8cc143f9eac103ec42f8bfe622a6095996e58895
Sebastian Trzpiot <mail@trzpiot.de>
2018-08-07 15:51:15 +0200

    Add an attribute notes to budget

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/persistence/budget/BudgetEntity.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/persistence/budget/BudgetEntity.java
@@ -34,6 +34,11 @@ public class BudgetEntity {
 
     private String description;
 
+    // Notes have a maximum size of 10MB
+    @Lob
+    @Column(length = 10485760)
+    private String note;
+
     private Money total;
 
     @ManyToOne(optional = false)
@@ -72,6 +77,7 @@ public class BudgetEntity {
         if (importKey != null ? !importKey.equals(that.importKey) : that.importKey != null) return false;
         if (name != null ? !name.equals(that.name) : that.name != null) return false;
         if (description != null ? !description.equals(that.description) : that.description != null) return false;
+        if (note != null ? !note.equals(that.note) : that.note != null) return false;
         if (planRecords != null ? !planRecords.equals(that.planRecords) : that.planRecords != null) return false;
         if (project != null ? !project.equals(that.project) : that.project != null) return false;
         if (tags != null ? !tags.equals(that.tags) : that.tags != null) return false;
@@ -86,6 +92,7 @@ public class BudgetEntity {
         int result = (int) (id ^ (id >>> 32));
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (note != null ? note.hashCode() : 0);
         result = 31 * result + (total != null ? total.hashCode() : 0);
         result = 31 * result + (project != null ? project.hashCode() : 0);
         result = 31 * result + (tags != null ? tags.hashCode() : 0);
