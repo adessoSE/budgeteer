269d6b3bbcd4ae0e2fcf0c11576f45a504a60175
Sebastian Trzpiot <mail@trzpiot.de>
2018-08-20 15:09:08 +0200

    Add calculation of current dates for the example file example_ubw_report

--- budgeteer-ubw-importer/src/main/java/org/wickedsource/budgeteer/importer/ubw/UBWWorkRecordsImporter.java
+++ budgeteer-ubw-importer/src/main/java/org/wickedsource/budgeteer/importer/ubw/UBWWorkRecordsImporter.java
@@ -1,12 +1,12 @@
 package org.wickedsource.budgeteer.importer.ubw;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
 import org.wickedsource.budgeteer.imports.api.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.*;
 
@@ -37,7 +37,7 @@ public class UBWWorkRecordsImporter implements WorkRecordsImporter {
 
             List<ImportedWorkRecord> resultList = new ArrayList<ImportedWorkRecord>();
             Workbook workbook = new XSSFWorkbook(file.getInputStream());
-            if(!checkValidity(workbook)){
+            if (!checkValidity(workbook)) {
                 throw new InvalidFileFormatException("Invalid file", file.getFilename());
             }
             Sheet sheet = workbook.getSheetAt(SHEET_INDEX);
@@ -48,7 +48,7 @@ public class UBWWorkRecordsImporter implements WorkRecordsImporter {
                     ImportedWorkRecord record = parseRow(row, file);
                     resultList.add(record);
                 } else {
-                    if(!isCompletelyEmpty(row)) {
+                    if (!isCompletelyEmpty(row)) {
                         skippedRecords.add(getRowAsStrings(row, i));
                     }
                 }
@@ -62,7 +62,7 @@ public class UBWWorkRecordsImporter implements WorkRecordsImporter {
     }
 
     private boolean isCompletelyEmpty(Row row) {
-        for(short i=row.getFirstCellNum(); i<row.getLastCellNum(); i++) {
+        for (short i = row.getFirstCellNum(); i < row.getLastCellNum(); i++) {
             Cell cell = row.getCell(i);
             if (!isBlank(cell.toString())) {
                 return false;
@@ -71,7 +71,7 @@ public class UBWWorkRecordsImporter implements WorkRecordsImporter {
         return true;
     }
 
-    private boolean isBlank(String s){
+    private boolean isBlank(String s) {
         return s == null || s.trim().isEmpty();
     }
 
@@ -89,15 +89,76 @@ public class UBWWorkRecordsImporter implements WorkRecordsImporter {
     public ExampleFile getExampleFile() {
         ExampleFile file = new ExampleFile();
         file.setFileName("ubw_report.xlsx");
-        file.setInputStream(getClass().getResourceAsStream("/example_ubw_report.xlsx"));
         file.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        try {
+            Date date = new Date();
+            Calendar maxCalendar = Calendar.getInstance();
+            Calendar maxineCalendar = Calendar.getInstance();
+            XSSFWorkbook workbook = new XSSFWorkbook(getClass().getResourceAsStream("/example_ubw_report.xlsx"));
+            XSSFSheet sheet = workbook.getSheetAt(2);
+            XSSFRow row;
+            XSSFCell cell;
+            int col = 3;
+            int i = sheet.getLastRowNum();
+
+            XSSFCellStyle style = workbook.createCellStyle();
+            style.setBorderBottom(BorderStyle.THIN);
+            style.setBorderTop(BorderStyle.THIN);
+            style.setBorderRight(BorderStyle.THIN);
+            style.setBorderLeft(BorderStyle.THIN);
+            style.setDataFormat((short) 14);
+
+            Iterator<Row> rows = sheet.rowIterator();
+
+            maxCalendar.setTime(date);
+            maxineCalendar.setTime(date);
+
+            while (i != 0) {
+                row = sheet.getRow(i);
+
+                if (row.getCell(col - 1).getStringCellValue().equals("Mustermann, Max")) {
+                    if (maxCalendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
+                        maxCalendar.add(Calendar.DATE, -1);
+                    } else if (maxCalendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+                        maxCalendar.add(Calendar.DATE, -2);
+                    }
+                    cell = row.createCell(3);
+                    cell.setCellValue(maxCalendar.getTime());
+                    cell.setCellStyle(style);
+                    maxCalendar.add(Calendar.DATE, -1);
+                } else if (row.getCell(col - 1).getStringCellValue().equals("Mustermann, Maxine")) {
+                    if (maxineCalendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
+                        maxineCalendar.add(Calendar.DATE, -1);
+                    } else if (maxineCalendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+                        maxineCalendar.add(Calendar.DATE, -2);
+                    }
+                    cell = row.createCell(3);
+                    cell.setCellValue(maxineCalendar.getTime());
+                    cell.setCellStyle(style);
+                    maxineCalendar.add(Calendar.DATE, -1);
+                }
+
+                i--;
+            }
+
+            File tmp = File.createTempFile("example_ubw_report", ".xlsx");
+            FileOutputStream fileOutputStream = new FileOutputStream(tmp);
+            workbook.write(fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+            file.setInputStream(new FileInputStream(tmp));
+        } catch (IOException e) {
+            e.printStackTrace();
+            file.setInputStream(getClass().getResourceAsStream("/example_ubw_report.xlsx"));
+        }
         return file;
     }
 
     @Override
     public List<List<String>> getSkippedRecords() {
         //if just an empty row at the beginning and the filename is in the List of skipped records, return an empty List
-        if(skippedRecords != null && skippedRecords.size() == 2){
+        if (skippedRecords != null && skippedRecords.size() == 2) {
             skippedRecords = new LinkedList<List<String>>();
         }
         return skippedRecords;
@@ -105,21 +166,21 @@ public class UBWWorkRecordsImporter implements WorkRecordsImporter {
 
 
     boolean checkValidity(Workbook workbook) {
-        boolean isValid = workbook.getNumberOfSheets() >= SHEET_INDEX ;
-        if(isValid){
+        boolean isValid = workbook.getNumberOfSheets() >= SHEET_INDEX;
+        if (isValid) {
             Sheet sheet = workbook.getSheetAt(SHEET_INDEX);
             int headerRowIndex = 2;
-            if(sheet.getRow(headerRowIndex) == null){
-                isValid = false ;
+            if (sheet.getRow(headerRowIndex) == null) {
+                isValid = false;
             } else {
                 Row r = sheet.getRow(headerRowIndex);
                 try {
                     isValid = r.getCell(COLUMN_PERSON).getStringCellValue().equals("Name") &&
                             r.getCell(COLUMN_DATE).getStringCellValue().equals("Tag") &&
                             r.getCell(COLUMN_BUDGET).getStringCellValue().equals("Subgruppe") &&
-                            r.getCell(COLUMN_HOURS).getStringCellValue().equals("Aufwand [h]")&&
+                            r.getCell(COLUMN_HOURS).getStringCellValue().equals("Aufwand [h]") &&
                             r.getCell(COLUMN_INVOICABLE).getStringCellValue().equals("KV");
-                }catch (Exception e){
+                } catch (Exception e) {
                     isValid = false;
                 }
             }
@@ -129,9 +190,9 @@ public class UBWWorkRecordsImporter implements WorkRecordsImporter {
 
     private List<String> getRowAsStrings(Row row, int index) {
         List<String> result = new LinkedList<String>();
-        for(short i=row.getFirstCellNum(); i<row.getLastCellNum(); i++) {
+        for (short i = row.getFirstCellNum(); i < row.getLastCellNum(); i++) {
             Cell cell = row.getCell(i);
-            if(cell == null) {
+            if (cell == null) {
                 result.add("");
                 continue;
             }
