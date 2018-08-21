5e51358dd72ce996e5b82fb0b9b1be865d228309
Sebastian Trzpiot <mail@trzpiot.de>
2018-08-21 16:06:13 +0200

    Expand register page with mail

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/user/register/RegisterPage.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/user/register/RegisterPage.java
@@ -1,11 +1,13 @@
 package org.wickedsource.budgeteer.web.pages.user.register;
 
 import org.apache.wicket.injection.Injector;
+import org.apache.wicket.markup.html.form.EmailTextField;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.wickedsource.budgeteer.service.user.MailAlreadyInUseException;
 import org.wickedsource.budgeteer.service.user.UserService;
 import org.wickedsource.budgeteer.service.user.UsernameAlreadyInUseException;
 import org.wickedsource.budgeteer.web.Mount;
@@ -27,21 +29,24 @@ public class RegisterPage extends DialogPage {
         Form<RegistrationData> form = new Form<RegistrationData>("registrationForm", model(from(new RegistrationData()))) {
             @Override
             protected void onSubmit() {
-                if(!getModelObject().getPassword().equals(getModelObject().getPasswordConfirmation())){
+                if (!getModelObject().getPassword().equals(getModelObject().getPasswordConfirmation())) {
                     error(getString("message.wrongPasswordConfirmation"));
                     return;
                 }
                 try {
-                    service.registerUser(getModelObject().getUsername(), getModelObject().getPassword());
+                    service.registerUser(getModelObject().getUsername(), getModelObject().getMail(), getModelObject().getPassword());
                     setResponsePage(LoginPage.class);
-                } catch(UsernameAlreadyInUseException e){
+                } catch (UsernameAlreadyInUseException e) {
                     this.error(getString("message.duplicateUserName"));
+                } catch (MailAlreadyInUseException e) {
+                    this.error(getString("message.duplicateMail"));
                 }
             }
         };
         add(form);
         form.add(new CustomFeedbackPanel("feedback"));
         form.add(new RequiredTextField<String>("username", model(from(form.getModel()).getUsername())));
+        form.add(new EmailTextField("mail", model(from(form.getModel()).getMail())).setRequired(true));
         form.add(new PasswordTextField("password", model(from(form.getModel()).getPassword())));
         form.add(new PasswordTextField("passwordConfirmation", model(from(form.getModel()).getPasswordConfirmation())));
         form.add(new BookmarkablePageLink<LoginPage>("loginLink", LoginPage.class));
