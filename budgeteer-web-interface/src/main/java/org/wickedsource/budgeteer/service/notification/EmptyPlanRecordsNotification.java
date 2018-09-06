001199da79c7536fb0d736ca5b282895090324df
Sebastian Trzpiot <mail@trzpiot.de>
2018-09-06 09:48:40 +0200

    Add annotations to notifications

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/service/notification/EmptyPlanRecordsNotification.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/service/notification/EmptyPlanRecordsNotification.java
@@ -1,5 +1,14 @@
 package org.wickedsource.budgeteer.service.notification;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class EmptyPlanRecordsNotification extends Notification {
 
 }
--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/service/notification/LimitReachedNotification.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/service/notification/LimitReachedNotification.java
@@ -1,28 +1,16 @@
 package org.wickedsource.budgeteer.service.notification;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
 public class LimitReachedNotification extends Notification {
 
     private long budgetId;
-
     private String budgetName;
 
     public LimitReachedNotification() {
         this.setNotificationType(NotificationType.info);
-    }
-
-    public long getBudgetId() {
-        return budgetId;
-    }
-
-    public void setBudgetId(long budgetId) {
-        this.budgetId = budgetId;
-    }
-
-    public String getBudgetName() {
-        return budgetName;
-    }
-
-    public void setBudgetName(String budgetName) {
-        this.budgetName = budgetName;
     }
 }
