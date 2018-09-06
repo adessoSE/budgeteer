64847690766bb956ea0a8550534238a7597d7198
Sebastian Trzpiot <mail@trzpiot.de>
2018-09-06 09:52:08 +0200

    Fix error

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/service/notification/EmptyPlanRecordsNotification.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/service/notification/EmptyPlanRecordsNotification.java
@@ -3,11 +3,9 @@ package org.wickedsource.budgeteer.service.notification;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 public class EmptyPlanRecordsNotification extends Notification {
 
