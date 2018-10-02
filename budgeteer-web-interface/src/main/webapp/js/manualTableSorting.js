afbde0d7c99dee90ce378d52ed5730fb395afe3f
adis <Leonie-Camilla.Adis@adesso.de>
2018-10-02 08:37:34 +0200

    Added script for manual table sorting (ContractOverview)

--- budgeteer-web-interface/src/main/webapp/js/manualTableSorting.js
+++ budgeteer-web-interface/src/main/webapp/js/manualTableSorting.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
    $("th").click(function(event){
         var source = $(event.target);
-   });
 
+        setRowIds();
+   });
 });
 
 function dragStart(event)
@@ -19,10 +20,32 @@ function drop()
 {
 }
 
-function down(event)}
+function setRowIds()
 {
+       var allRows = $("table").find(".odd, .even");
+
+       for(var i=0; i<allRows.length; i++)
+       {
+            $(allRows[i]).attr("id", i);
+       }
+}
+
+function down(event)
+{
+    setRowIds();
     var source = event.target || event.srcElement;
+    var currentRow = $(source).parents("tr");
+    var currentRowId = $(currentRow).attr('id');
 
 
+    var allRows = $("table").find(".odd, .even");
+
+   for(var i=0; i<allRows.length; i++)
+   {
+        if($(allRows[i]).attr('id') == currentRowId))
+        {
+
+        }
+   }
 }
 
