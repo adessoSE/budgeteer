faef31bcd21779daeec260517c6dfb9ef01fd6aa
maximAtanasov <Maksim.Atanasov@adesso.de>
2019-01-04 11:35:47 +0100

    replaced getters and setters in LoginCredentials with lombok data anotaion

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/user/login/LoginCredentials.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/user/login/LoginCredentials.java
@@ -1,26 +1,13 @@
 package org.wickedsource.budgeteer.web.pages.user.login;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class LoginCredentials implements Serializable {
 
     private String username;
 
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
