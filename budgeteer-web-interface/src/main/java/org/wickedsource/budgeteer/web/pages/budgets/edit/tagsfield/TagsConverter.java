efc3270c6c5c6151adafeb1c0eb102b4185dec09
Florian Czarnecki <Florian.Czarnecki@adesso.de>
2021-07-16 12:29:26 +0200

    Fix TagsConverter#convertToString returning a list of empty string
    TagsConverter would return a list containing only the empty string when
    the input was empty. This of course isn't the expected behaviour.

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/budgets/edit/tagsfield/TagsConverter.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/budgets/edit/tagsfield/TagsConverter.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.util.convert.ConversionException;
 import org.apache.wicket.util.convert.IConverter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -12,6 +13,9 @@ public class TagsConverter implements IConverter<List<String>> {
 
     @Override
     public List<String> convertToObject(String value, Locale locale) throws ConversionException {
+        if (value == null || value.trim().isEmpty()) {
+            return new ArrayList<>();
+        }
         String[] values = value.split(",");
         return Arrays.asList(values);
     }
