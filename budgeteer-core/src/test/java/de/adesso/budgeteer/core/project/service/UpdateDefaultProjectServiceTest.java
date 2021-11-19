4c98f6b63f1c10a4ba7cdec4d7c6904ee6d4b524
Florian Czarnecki <Florian.Czarnecki@adesso.de>
2021-11-19 13:20:48 +0100

    project: Fix broken UpdateDefaultProjectTest and add missing one

--- budgeteer-core/src/test/java/de/adesso/budgeteer/core/project/service/UpdateDefaultProjectServiceTest.java
+++ budgeteer-core/src/test/java/de/adesso/budgeteer/core/project/service/UpdateDefaultProjectServiceTest.java
@@ -1,5 +1,7 @@
 package de.adesso.budgeteer.core.project.service;
 
+import de.adesso.budgeteer.core.project.domain.Project;
+import de.adesso.budgeteer.core.project.port.out.ProjectExistsWithIdPort;
 import de.adesso.budgeteer.core.project.port.out.UpdateDefaultProjectPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,23 +9,37 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UpdateDefaultProjectServiceTest {
 
     @InjectMocks private UpdateDefaultProjectService updateDefaultProjectService;
     @Mock private UpdateDefaultProjectPort updateDefaultProjectPort;
+    @Mock private ProjectExistsWithIdPort projectExistsWithIdPort;
 
     @Test
     void shouldUpdateDefaultProject() {
         var userId = 1L;
         var projectId = 2L;
-        doNothing().when(updateDefaultProjectPort).updateDefaultProject(userId, projectId);
+        var expected = new Project(projectId, "name");
+        when(updateDefaultProjectPort.updateDefaultProject(userId, projectId)).thenReturn(expected);
+        when(projectExistsWithIdPort.projectExistsWithId(projectId)).thenReturn(true);
 
-        updateDefaultProjectService.updateDefaultProject(userId, projectId);
+        var returned = updateDefaultProjectService.updateDefaultProject(userId, projectId);
 
-        verify(updateDefaultProjectPort).updateDefaultProject(userId, projectId);
+        assertThat(returned).contains(expected);
+    }
+
+    @Test
+    void shouldReturnNothingWhenDefaultProjectDoesNotExist() {
+        var userId = 1L;
+        var projectId = 2L;
+        when(projectExistsWithIdPort.projectExistsWithId(projectId)).thenReturn(false);
+
+        var returned = updateDefaultProjectService.updateDefaultProject(userId, projectId);
+
+        assertThat(returned).isEmpty();
     }
 }
