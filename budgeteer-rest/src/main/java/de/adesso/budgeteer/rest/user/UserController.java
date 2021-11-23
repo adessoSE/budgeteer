eedc6df93ece4556e51fd1391f7f0f6385e11301
Florian Czarnecki <florian.czarnecki@adesso.de>
2021-11-23 10:48:25 +0100

    user: Add RegistrationController

---
+++ budgeteer-rest/src/main/java/de/adesso/budgeteer/rest/user/UserController.java
@@ -0,0 +1,26 @@
+package de.adesso.budgeteer.rest.user;
+
+import de.adesso.budgeteer.core.user.MailAlreadyInUseException;
+import de.adesso.budgeteer.core.user.UsernameAlreadyInUseException;
+import de.adesso.budgeteer.core.user.port.in.RegisterUseCase;
+import de.adesso.budgeteer.rest.user.model.RegisterModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final RegisterUseCase registerUseCase;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public void register(@RequestBody @Valid RegisterModel registerModel) throws UsernameAlreadyInUseException, MailAlreadyInUseException {
+        registerUseCase.register(new RegisterUseCase.RegisterCommand(registerModel.getUsername(), registerModel.getEmail(), passwordEncoder.encode(registerModel.getPassword())));
+    }
+}
