0da9495cf6145c33b85a59d0797a7e74736f107a
Florian Czarnecki <florian.czarnecki@adesso.de>
2021-11-23 14:26:56 +0100

    fixup! budget: Add budget controller

--- budgeteer-rest/src/main/java/de/adesso/budgeteer/rest/budget/model/CreateBudgetModel.java
+++ budgeteer-rest/src/main/java/de/adesso/budgeteer/rest/budget/model/CreateBudgetModel.java
@@ -3,16 +3,32 @@ package de.adesso.budgeteer.rest.budget.model;
 import lombok.Data;
 import org.joda.money.Money;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class CreateBudgetModel {
+    @Positive
     long projectId;
+
+    @Positive
     Long contractId;
+
+    @NotEmpty
     String name;
+
     String description;
+
+    @NotEmpty
     String importKey;
+
+    @NotNull
     Money total;
+
     Money limit;
-    List<String> tags;
+
+    List<String> tags = new ArrayList<>();
 }
