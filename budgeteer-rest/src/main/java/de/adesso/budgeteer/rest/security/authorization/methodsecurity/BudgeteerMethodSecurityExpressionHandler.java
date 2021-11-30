6d26dd53d4565db38f965a884ad3c4f4202c6fee
Maximilian Korte <maximilian.korte@adesso.de>
2021-11-30 14:39:20 +0100

    Add methods for method security

--- budgeteer-rest/src/main/java/de/adesso/budgeteer/rest/security/authorization/methodsecurity/BudgeteerMethodSecurityExpressionHandler.java
+++ budgeteer-rest/src/main/java/de/adesso/budgeteer/rest/security/authorization/methodsecurity/BudgeteerMethodSecurityExpressionHandler.java
@@ -1,5 +1,9 @@
 package de.adesso.budgeteer.rest.security.authorization.methodsecurity;
 
+import de.adesso.budgeteer.core.budget.port.in.UserHasAccessToBudgetUseCase;
+import de.adesso.budgeteer.core.contract.port.in.UserHasAccessToContractUseCase;
+import de.adesso.budgeteer.core.invoice.port.in.UserHasAccessToInvoiceUseCase;
+import de.adesso.budgeteer.core.person.port.in.UserHasAccessToPersonUseCase;
 import de.adesso.budgeteer.core.project.port.in.UserHasAccessToProjectUseCase;
 import lombok.RequiredArgsConstructor;
 import org.aopalliance.intercept.MethodInvocation;
@@ -13,10 +17,14 @@ import org.springframework.security.core.Authentication;
 public class BudgeteerMethodSecurityExpressionHandler extends DefaultMethodSecurityExpressionHandler {
 
     private final UserHasAccessToProjectUseCase userHasAccessToProjectUseCase;
+    private final UserHasAccessToContractUseCase userHasAccessToContractUseCase;
+    private final UserHasAccessToInvoiceUseCase userHasAccessToInvoiceUseCase;
+    private final UserHasAccessToBudgetUseCase userHasAccessToBudgetUseCase;
+    private final UserHasAccessToPersonUseCase userHasAccessToPersonUseCase;
 
     @Override
     protected MethodSecurityExpressionOperations createSecurityExpressionRoot(Authentication authentication, MethodInvocation invocation) {
-        var root = new BudgeteerMethodSecurityExpressionRoot(authentication, userHasAccessToProjectUseCase);
+        var root = new BudgeteerMethodSecurityExpressionRoot(authentication, userHasAccessToProjectUseCase, userHasAccessToContractUseCase, userHasAccessToInvoiceUseCase, userHasAccessToBudgetUseCase, userHasAccessToPersonUseCase);
         root.setPermissionEvaluator(new DenyAllPermissionEvaluator());
         root.setTrustResolver(new AuthenticationTrustResolverImpl());
         return root;
