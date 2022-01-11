778187ddfa321c8801c5c974a55fa55a90fa0442
Florian Czarnecki <florian.czarnecki@adesso.de>
2022-01-11 11:35:58 +0100

    rest: Remove controller advice that is only applicable one controller

--- budgeteer-rest/src/main/java/de/adesso/budgeteer/rest/project/ProjectController.java
+++ budgeteer-rest/src/main/java/de/adesso/budgeteer/rest/project/ProjectController.java
@@ -2,6 +2,8 @@ package de.adesso.budgeteer.rest.project;
 
 import de.adesso.budgeteer.core.project.ProjectException;
 import de.adesso.budgeteer.core.project.port.in.*;
+import de.adesso.budgeteer.rest.project.errors.CreateProjectError;
+import de.adesso.budgeteer.rest.project.errors.UpdateProjectError;
 import de.adesso.budgeteer.rest.project.exceptions.CreateProjectException;
 import de.adesso.budgeteer.rest.project.exceptions.UpdateProjectException;
 import de.adesso.budgeteer.rest.project.model.*;
@@ -13,6 +15,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -128,5 +131,31 @@ public class ProjectController {
       @PathVariable("projectId") ProjectIdModel projectIdModel) {
     return projectModelMapper.toModel(
         getProjectWithDateUseCase.getProjectWithDate(projectIdModel.getValue()));
+  }
+
+  @ExceptionHandler(CreateProjectException.class)
+  public ResponseEntity<CreateProjectError> handleCreateProjectException(
+      CreateProjectException createProjectException) {
+    var causes = createProjectException.getProjectException().getCauses().getAll();
+
+    var createProjectError = new CreateProjectError();
+    createProjectError.setNamealreadyInUse(
+        causes.contains(ProjectException.ProjectErrors.NAME_ALREADY_IN_USE));
+
+    return ResponseEntity.badRequest().body(createProjectError);
+  }
+
+  @ExceptionHandler(UpdateProjectException.class)
+  public ResponseEntity<UpdateProjectError> handleUpdateProjectException(
+      UpdateProjectException updateProjectException) {
+    var causes = updateProjectException.getProjectException().getCauses().getAll();
+
+    var updateProjectError = new UpdateProjectError();
+    updateProjectError.setProjectNotFound(
+        causes.contains(ProjectException.ProjectErrors.PROJECT_NOT_FOUND));
+    updateProjectError.setNameAlreadyInUse(
+        causes.contains(ProjectException.ProjectErrors.NAME_ALREADY_IN_USE));
+
+    return ResponseEntity.badRequest().body(updateProjectError);
   }
 }
--- budgeteer-rest/src/main/java/de/adesso/budgeteer/rest/user/UserController.java
+++ budgeteer-rest/src/main/java/de/adesso/budgeteer/rest/user/UserController.java
@@ -4,16 +4,15 @@ import de.adesso.budgeteer.core.user.UserException;
 import de.adesso.budgeteer.core.user.domain.User;
 import de.adesso.budgeteer.core.user.port.in.GetUserByNameUseCase;
 import de.adesso.budgeteer.core.user.port.in.RegisterUseCase;
+import de.adesso.budgeteer.rest.user.errors.RegisterUserError;
 import de.adesso.budgeteer.rest.user.exceptions.RegisterUserException;
 import de.adesso.budgeteer.rest.user.model.RegisterModel;
 import java.security.Principal;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -41,5 +40,19 @@ public class UserController {
     return getUserByNameUseCase
         .getUserByName(principal.getName())
         .orElseThrow(IllegalArgumentException::new);
+  }
+
+  @ExceptionHandler(RegisterUserException.class)
+  public ResponseEntity<RegisterUserError> handleRegisterUserException(
+      RegisterUserException registerUserException) {
+    var causes = registerUserException.getUserException().getCauses();
+
+    var registerUserError = new RegisterUserError();
+    registerUserError.setUsernameAlreadyInUse(
+        causes.contains(UserException.UserErrors.USERNAME_ALREADY_IN_USE));
+    registerUserError.setMailAlreadyInUse(
+        causes.contains(UserException.UserErrors.MAIL_ALREADY_IN_USE));
+
+    return ResponseEntity.badRequest().body(registerUserError);
   }
 }
