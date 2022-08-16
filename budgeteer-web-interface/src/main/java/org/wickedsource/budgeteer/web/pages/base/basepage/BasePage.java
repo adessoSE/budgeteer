1110b884aa513f019b70bbd393525c01cc34b472
Florian Czarnecki <florian.czarnecki@adesso.de>
2022-08-16 12:55:46 +0200

    administration: Make administer project button visible
    I don't know why it was hidden in the first place, but everybody with
    access to the project should be able to see the button, since they can
    just adjust the URL anyway.

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/base/basepage/BasePage.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/base/basepage/BasePage.java
@@ -30,103 +30,112 @@ import org.wickedsource.budgeteer.web.pages.user.selectproject.SelectProjectPage
 @NeedsLogin
 @NeedsProject
 public abstract class BasePage extends WebPage {
-    protected NotificationDropdown notificationDropdown;
+  protected NotificationDropdown notificationDropdown;
 
-    @SpringBean
-    private BudgeteerSettings settings;
+  @SpringBean private BudgeteerSettings settings;
 
-    public BasePage(PageParameters parameters) {
-        super(parameters);
-        addComponents();
+  public BasePage(PageParameters parameters) {
+    super(parameters);
+    addComponents();
+  }
+
+  public BasePage() {
+    addComponents();
+  }
+
+  /**
+   * Creates a valid PageParameters object to pass into the constructor of this page class.
+   *
+   * @param objectId id of the budget whose details to display.
+   * @return a valid PageParameters object.
+   */
+  public static PageParameters createParameters(long objectId) {
+    PageParameters parameters = new PageParameters();
+    parameters.add("id", objectId);
+    return parameters;
+  }
+
+  @SuppressWarnings("unchecked")
+  private void addComponents() {
+    BreadcrumbsPanel breadcrumbs = new BreadcrumbsPanel("breadcrumbsPanel", getBreadcrumbsModel());
+    long projectId = ((BudgeteerSession) getSession()).getProjectId();
+    long userId = ((BudgeteerSession) getSession()).getLoggedInUser().getId();
+    add(breadcrumbs);
+    notificationDropdown =
+        new NotificationDropdown("notificationDropdown", new NotificationModel(projectId, userId));
+    notificationDropdown.setOutputMarkupId(true);
+    add(notificationDropdown);
+    add(new BudgetUnitChoice("budgetUnitDropdown", new BudgetUnitModel(projectId)));
+
+    BookmarkablePageLink pageLink =
+        new BookmarkablePageLink<ProjectAdministrationPage>(
+            "administrationLink", ProjectAdministrationPage.class);
+    add(pageLink);
+    pageLink.setVisible(true);
+    add(createProjectChangeLink("changeProjectLink"));
+    add(createMyProfileLink("myProfileLink"));
+    add(createLogoutLink("logoutLink"));
+    add(createDashboardLink("dashboardLink"));
+    add(new HeaderResponseContainer("JavaScriptContainer", "JavaScriptContainer"));
+  }
+
+  @Override
+  public void renderHead(IHeaderResponse response) {
+    super.renderHead(response);
+    response.render(
+        JavaScriptReferenceHeaderItem.forReference(BudgeteerReferences.getJQueryReference()));
+    response.render(
+        JavaScriptReferenceHeaderItem.forReference(BudgeteerReferences.getAdminLteAppReference()));
+  }
+
+  private Component createDashboardLink(String id) {
+    return new BookmarkablePageLink<>(id, DashboardPage.class);
+  }
+
+  private Link createProjectChangeLink(String id) {
+    return new Link<Void>(id) {
+      @Override
+      public void onClick() {
+        setResponsePage(new SelectProjectPage(this.getWebPage().getClass(), getPageParameters()));
+      }
+    };
+  }
+
+  private Link createLogoutLink(String id) {
+    return new Link<Void>(id) {
+      @Override
+      public void onClick() {
+        BudgeteerSession.get().logout();
+        setResponsePage(LoginPage.class);
+      }
+    };
+  }
+
+  private Link createMyProfileLink(String id) {
+    return new Link<Void>(id) {
+      @Override
+      public void onClick() {
+        setResponsePage(
+            new EditUserPage(
+                this.getWebPage().getClass(),
+                new PageParameters()
+                    .add("userId", BudgeteerSession.get().getLoggedInUser().getId())));
+      }
+    };
+  }
+
+  /**
+   * Expects a BreadcrumbsModel to be returned that contains all pages that should be displayed in
+   * the breadcrumbs.
+   */
+  protected abstract BreadcrumbsModel getBreadcrumbsModel();
+
+  public long getParameterId() {
+    StringValue value = getPageParameters().get("id");
+    if (value == null || value.isEmpty() || value.isNull()) {
+      return 0L;
+    } else {
+      return value.toLong();
     }
-
-    public BasePage() {
-        addComponents();
-    }
-
-    /**
-     * Creates a valid PageParameters object to pass into the constructor of this page class.
-     *
-     * @param objectId id of the budget whose details to display.
-     * @return a valid PageParameters object.
-     */
-    public static PageParameters createParameters(long objectId) {
-        PageParameters parameters = new PageParameters();
-        parameters.add("id", objectId);
-        return parameters;
-    }
-
-    @SuppressWarnings("unchecked")
-    private void addComponents() {
-        BreadcrumbsPanel breadcrumbs = new BreadcrumbsPanel("breadcrumbsPanel", getBreadcrumbsModel());
-        long projectId = ((BudgeteerSession) getSession()).getProjectId();
-        long userId = ((BudgeteerSession) getSession()).getLoggedInUser().getId();
-        add(breadcrumbs);
-        notificationDropdown = new NotificationDropdown("notificationDropdown", new NotificationModel(projectId, userId));
-        notificationDropdown.setOutputMarkupId(true);
-        add(notificationDropdown);
-        add(new BudgetUnitChoice("budgetUnitDropdown", new BudgetUnitModel(projectId)));
-
-        BookmarkablePageLink pageLink = new BookmarkablePageLink<ProjectAdministrationPage>("administrationLink", ProjectAdministrationPage.class);
-        add(pageLink);
-        pageLink.setVisible(false);
-        add(createProjectChangeLink("changeProjectLink"));
-        add(createMyProfileLink("myProfileLink"));
-        add(createLogoutLink("logoutLink"));
-        add(createDashboardLink("dashboardLink"));
-        add(new HeaderResponseContainer("JavaScriptContainer", "JavaScriptContainer"));
-    }
-
-    @Override
-    public void renderHead(IHeaderResponse response) {
-        super.renderHead(response);
-        response.render(JavaScriptReferenceHeaderItem.forReference(BudgeteerReferences.getJQueryReference()));
-        response.render(JavaScriptReferenceHeaderItem.forReference(BudgeteerReferences.getAdminLteAppReference()));
-    }
-
-    private Component createDashboardLink(String id) {
-        return new BookmarkablePageLink<>(id, DashboardPage.class);
-    }
-
-    private Link createProjectChangeLink(String id) {
-        return new Link<Void>(id) {
-            @Override
-            public void onClick() {
-                setResponsePage(new SelectProjectPage(this.getWebPage().getClass(), getPageParameters()));
-            }
-        };
-    }
-
-    private Link createLogoutLink(String id) {
-        return new Link<Void>(id) {
-            @Override
-            public void onClick() {
-                BudgeteerSession.get().logout();
-                setResponsePage(LoginPage.class);
-            }
-        };
-    }
-
-    private Link createMyProfileLink(String id) {
-        return new Link<Void>(id) {
-            @Override
-            public void onClick() {
-                setResponsePage(new EditUserPage(this.getWebPage().getClass(), new PageParameters().add("userId", BudgeteerSession.get().getLoggedInUser().getId())));
-            }
-        };
-    }
-
-    /**
-     * Expects a BreadcrumbsModel to be returned that contains all pages that should be displayed in the breadcrumbs.
-     */
-    protected abstract BreadcrumbsModel getBreadcrumbsModel();
-
-    public long getParameterId() {
-        StringValue value = getPageParameters().get("id");
-        if (value == null || value.isEmpty() || value.isNull()) {
-            return 0L;
-        } else {
-            return value.toLong();
-        }
-    }
+  }
 }
