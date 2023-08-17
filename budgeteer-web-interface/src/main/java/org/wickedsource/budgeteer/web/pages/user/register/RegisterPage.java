16f741974c2c048ca6c9f15271f6f5c5221b9e8b
Florian Czarnecki <florian.czarnecki@adesso.de>
2023-08-17 13:18:51 +0200

    Cleanup RegisterPage

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/user/register/RegisterPage.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/user/register/RegisterPage.java
@@ -1,14 +1,13 @@
 package org.wickedsource.budgeteer.web.pages.user.register;
 
-import static org.wicketstuff.lazymodel.LazyModel.from;
-import static org.wicketstuff.lazymodel.LazyModel.model;
+import static org.apache.wicket.model.LambdaModel.of;
 
-import org.apache.wicket.injection.Injector;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.form.validation.EqualPasswordInputValidator;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wickedsource.budgeteer.service.user.UserService;
 import org.wickedsource.budgeteer.service.user.UsernameAlreadyInUseException;
@@ -23,33 +22,38 @@ public class RegisterPage extends DialogPage {
   @SpringBean private UserService service;
 
   public RegisterPage() {
-    Injector.get().inject(this);
-    Form<RegistrationData> form =
-        new Form<RegistrationData>("registrationForm", model(from(new RegistrationData()))) {
+    var form =
+        new Form<>("registrationForm", Model.of(new RegistrationData())) {
           @Override
           protected void onSubmit() {
-            if (!getModelObject()
-                .getPassword()
-                .equals(getModelObject().getPasswordConfirmation())) {
-              error(getString("message.wrongPasswordConfirmation"));
-              return;
-            }
             try {
               service.registerUser(getModelObject().getUsername(), getModelObject().getPassword());
-              setResponsePage(
-                  LoginPage.class, new PageParameters().add("verificationsent", "true"));
+              setResponsePage(LoginPage.class);
             } catch (UsernameAlreadyInUseException e) {
               this.error(getString("message.duplicateUserName"));
             }
           }
         };
-    add(form);
     form.add(new CustomFeedbackPanel("feedback"));
-    form.add(new RequiredTextField<String>("username", model(from(form.getModel()).getUsername())));
-    form.add(new PasswordTextField("password", model(from(form.getModel()).getPassword())));
     form.add(
+        new RequiredTextField<>(
+            "username",
+            of(form.getModel(), RegistrationData::getUsername, RegistrationData::setUsername)));
+    var passwordField =
         new PasswordTextField(
-            "passwordConfirmation", model(from(form.getModel()).getPasswordConfirmation())));
+            "password",
+            of(form.getModel(), RegistrationData::getPassword, RegistrationData::setPassword));
+    var passwordConfirmationField =
+        new PasswordTextField(
+            "passwordConfirmation",
+            of(
+                form.getModel(),
+                RegistrationData::getPasswordConfirmation,
+                RegistrationData::setPasswordConfirmation));
+    form.add(passwordField);
+    form.add(passwordConfirmationField);
+    form.add(new EqualPasswordInputValidator(passwordField, passwordConfirmationField));
     form.add(new BookmarkablePageLink<LoginPage>("loginLink", LoginPage.class));
+    add(form);
   }
 }
