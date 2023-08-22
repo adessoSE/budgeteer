d91db88e5e8763bc0e34ddb0dbc1de61cbe47012
Florian Czarnecki <florian.czarnecki@adesso.de>
2023-08-22 12:17:23 +0200

    Rework EditContractPage

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/BudgeteerApplication.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/BudgeteerApplication.java
@@ -1,18 +1,21 @@
 package org.wickedsource.budgeteer.web;
 
 import de.adesso.wickedcharts.wicket8.JavaScriptResourceRegistry;
+import java.util.Date;
+import java.util.Set;
+import java.util.function.Supplier;
+import org.apache.wicket.ConverterLocator;
+import org.apache.wicket.IConverterLocator;
 import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.Session;
-import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.ResourceAggregator;
 import org.apache.wicket.markup.head.filter.JavaScriptFilteredIntoFooterHeaderResponse;
-import org.apache.wicket.markup.html.IHeaderResponseDecorator;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.request.IExceptionMapper;
 import org.apache.wicket.request.Request;
 import org.apache.wicket.request.Response;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.joda.money.Money;
 import org.reflections.Reflections;
 import org.reflections.scanners.TypeAnnotationsScanner;
 import org.reflections.util.ClasspathHelper;
@@ -21,101 +24,112 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+import org.wickedsource.budgeteer.web.components.daterange.DateConverter;
 import org.wickedsource.budgeteer.web.components.instantiation.BudgeteerRequiresProjectListener;
+import org.wickedsource.budgeteer.web.components.money.MoneyConverter;
 import org.wickedsource.budgeteer.web.components.security.BudgeteerAuthorizationStrategy;
 import org.wickedsource.budgeteer.web.components.security.BudgeteerUnauthorizedComponentInstantiationListener;
 import org.wickedsource.budgeteer.web.pages.dashboard.DashboardPage;
 
-import java.util.Set;
-import java.util.function.Supplier;
-
 @Component
 public class BudgeteerApplication extends WebApplication implements ApplicationContextAware {
 
-    private ApplicationContext context;
+  private ApplicationContext context;
 
-    @Autowired
-    private BudgeteerSettings settings;
+  @Autowired private BudgeteerSettings settings;
 
-    @Override
-    public Class<? extends WebPage> getHomePage() {
-        return DashboardPage.class;
+  @Override
+  public Class<? extends WebPage> getHomePage() {
+    return DashboardPage.class;
+  }
+
+  @Override
+  public void init() {
+    super.init();
+
+    getCspSettings().blocking().disabled();
+
+    getMarkupSettings().setStripWicketTags(true);
+    getComponentInstantiationListeners().add(new SpringComponentInjector(this, context));
+    initWickedCharts();
+    getJavaScriptLibrarySettings().setJQueryReference(BudgeteerReferences.getJQueryReference());
+    mountPages();
+
+    getSecuritySettings().setAuthorizationStrategy(new BudgeteerAuthorizationStrategy());
+    getSecuritySettings()
+        .setUnauthorizedComponentInstantiationListener(
+            new BudgeteerUnauthorizedComponentInstantiationListener());
+    getHeaderResponseDecorators()
+        .add(
+            response ->
+                new JavaScriptFilteredIntoFooterHeaderResponse(response, "JavaScriptContainer"));
+
+    // add component instantiation/onBeforeRender listener
+    final BudgeteerRequiresProjectListener listener = new BudgeteerRequiresProjectListener();
+    getComponentInstantiationListeners().add(listener);
+    getComponentPreOnBeforeRenderListeners().add(listener);
+  }
+
+  @Override
+  public RuntimeConfigurationType getConfigurationType() {
+    String configuration = settings.getConfigurationType();
+    if (RuntimeConfigurationType.DEVELOPMENT.name().equalsIgnoreCase(configuration)) {
+      return RuntimeConfigurationType.DEVELOPMENT;
+    } else {
+      return RuntimeConfigurationType.DEPLOYMENT;
     }
+  }
 
-    @Override
-    public void init() {
-        super.init();
+  /** Scans the classpath for all pages annotated with the Mount annotation and mounts them. */
+  @SuppressWarnings("unchecked")
+  private void mountPages() {
+    Reflections reflections =
+        new Reflections(
+            new ConfigurationBuilder()
+                .setUrls(ClasspathHelper.forPackage("org.wickedsource.budgeteer"))
+                .setScanners(new TypeAnnotationsScanner()));
+    Set<Class<?>> pagesToMount = reflections.getTypesAnnotatedWith(Mount.class, true);
 
-        getCspSettings().blocking().disabled();
-
-        getMarkupSettings().setStripWicketTags(true);
-        getComponentInstantiationListeners().add(new SpringComponentInjector(this, context));
-        initWickedCharts();
-        getJavaScriptLibrarySettings().setJQueryReference(BudgeteerReferences.getJQueryReference());
-        mountPages();
-
-        getSecuritySettings().setAuthorizationStrategy(new BudgeteerAuthorizationStrategy());
-        getSecuritySettings().setUnauthorizedComponentInstantiationListener(new BudgeteerUnauthorizedComponentInstantiationListener());
-        getHeaderResponseDecorators().add(response -> new JavaScriptFilteredIntoFooterHeaderResponse(response, "JavaScriptContainer"));
-
-        // add component instantiation/onBeforeRender listener
-        final BudgeteerRequiresProjectListener listener = new BudgeteerRequiresProjectListener();
-        getComponentInstantiationListeners().add(listener);
-        getComponentPreOnBeforeRenderListeners().add(listener);
+    for (Class<?> page : pagesToMount) {
+      Class<? extends WebPage> pageClass = (Class<? extends WebPage>) page;
+      Mount mount = pageClass.getAnnotation(Mount.class);
+      for (String mountUrl : mount.value()) {
+        mountPage(mountUrl, pageClass);
+      }
     }
+  }
 
-    @Override
-    public RuntimeConfigurationType getConfigurationType() {
-        String configuration = settings.getConfigurationType();
-        if (RuntimeConfigurationType.DEVELOPMENT.name().equalsIgnoreCase(configuration)) {
-            return RuntimeConfigurationType.DEVELOPMENT;
-        } else {
-            return RuntimeConfigurationType.DEPLOYMENT;
-        }
-    }
+  private void initWickedCharts() {
+    JavaScriptResourceRegistry.getInstance()
+        .setChartJsReference(BudgeteerReferences.getChartjsReference());
+    JavaScriptResourceRegistry.getInstance()
+        .setChartJsBundleReference(BudgeteerReferences.getChartjsReference());
+    JavaScriptResourceRegistry.getInstance()
+        .setMomentJsReference(BudgeteerReferences.getMomentJsReference());
+    JavaScriptResourceRegistry.getInstance()
+        .setJQueryReference(BudgeteerReferences.getJQueryReference());
+  }
 
-    /**
-     * Scans the classpath for all pages annotated with the Mount annotation and mounts them.
-     */
-    @SuppressWarnings("unchecked")
-    private void mountPages() {
-        Reflections reflections = new Reflections(
-                new ConfigurationBuilder().setUrls(
-                        ClasspathHelper.forPackage("org.wickedsource.budgeteer")).setScanners(
-                        new TypeAnnotationsScanner()));
-        Set<Class<?>> pagesToMount = reflections.getTypesAnnotatedWith(Mount.class, true);
+  @Override
+  public Session newSession(Request request, Response response) {
+    return new BudgeteerSession(request);
+  }
 
-        for (Class<?> page : pagesToMount) {
-            Class<? extends WebPage> pageClass = (Class<? extends WebPage>) page;
-            Mount mount = pageClass.getAnnotation(Mount.class);
-            for (String mountUrl : mount.value()) {
-                mountPage(mountUrl, pageClass);
-            }
-        }
-    }
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) {
+    this.context = applicationContext;
+  }
 
-    private void initWickedCharts() {
-        JavaScriptResourceRegistry.getInstance().setChartJsReference(BudgeteerReferences.getChartjsReference());
-        JavaScriptResourceRegistry.getInstance().setChartJsBundleReference(BudgeteerReferences.getChartjsReference());
-        JavaScriptResourceRegistry.getInstance().setMomentJsReference(BudgeteerReferences.getMomentJsReference());
-        JavaScriptResourceRegistry.getInstance().setJQueryReference(BudgeteerReferences.getJQueryReference());
-    }
+  @Override
+  public Supplier<IExceptionMapper> getExceptionMapperProvider() {
+    return BudgeteerExceptionMapper::new;
+  }
 
-    @Override
-    public Session newSession(Request request, Response response) {
-        return new BudgeteerSession(request);
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) {
-        this.context = applicationContext;
-    }
-
-    @Override
-    public Supplier<IExceptionMapper> getExceptionMapperProvider() {
-        return BudgeteerExceptionMapper::new;
-    }
-
+  @Override
+  protected IConverterLocator newConverterLocator() {
+    var converterLocator = new ConverterLocator();
+    converterLocator.set(Date.class, new DateConverter());
+    converterLocator.set(Money.class, new MoneyConverter());
+    return converterLocator;
+  }
 }
-
-
--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/components/daterange/DateConverter.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/components/daterange/DateConverter.java
@@ -1,29 +1,27 @@
 package org.wickedsource.budgeteer.web.components.daterange;
 
-import org.apache.wicket.util.convert.ConversionException;
-import org.apache.wicket.util.convert.IConverter;
-
 import java.text.ParseException;
 import java.util.Date;
 import java.util.Locale;
+import org.apache.wicket.util.convert.ConversionException;
+import org.apache.wicket.util.convert.IConverter;
 
 public class DateConverter implements IConverter<Date> {
-    @Override
-    public Date convertToObject(String value, Locale locale){
-        if(value.isEmpty())
-            return null;
-        try {
-           return DateInputField.format.parse(value);
-        } catch (ParseException e) {
-            throw new ConversionException(e);
-        }
+  @Override
+  public Date convertToObject(String value, Locale locale) {
+    if (value.isEmpty()) return null;
+    try {
+      return DateInputField.FORMAT.parse(value);
+    } catch (ParseException e) {
+      throw new ConversionException(e);
     }
+  }
 
-    @Override
-    public String convertToString(Date value, Locale locale) {
-        if(value == null){
-            return "";
-        }
-        return DateInputField.format.format(value);
+  @Override
+  public String convertToString(Date value, Locale locale) {
+    if (value == null) {
+      return "";
     }
+    return DateInputField.FORMAT.format(value);
+  }
 }
