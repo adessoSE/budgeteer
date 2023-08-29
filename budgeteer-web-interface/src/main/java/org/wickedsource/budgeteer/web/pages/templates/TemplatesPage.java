05a5b7d6640235678a502cee57da2b907abe8542
Florian Czarnecki <florian.czarnecki@adesso.de>
2023-08-29 14:42:30 +0200

    Rework TemplateFilterPanel

--- budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/templates/TemplatesPage.java
+++ budgeteer-web-interface/src/main/java/org/wickedsource/budgeteer/web/pages/templates/TemplatesPage.java
@@ -1,6 +1,7 @@
 package org.wickedsource.budgeteer.web.pages.templates;
 
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.Model;
 import org.wickedsource.budgeteer.web.BudgeteerSession;
 import org.wickedsource.budgeteer.web.Mount;
 import org.wickedsource.budgeteer.web.pages.base.basepage.BasePage;
@@ -13,25 +14,28 @@ import org.wickedsource.budgeteer.web.pages.templates.templateimport.ImportTempl
 @Mount("templates")
 public class TemplatesPage extends BasePage {
 
-    public TemplatesPage() {
-        TemplatesTable table = new TemplatesTable("templateTable", new TemplateListModel(BudgeteerSession.get().getTemplateFilter()));
-        add(table);
-        add(new TemplateFilterPanel("filter", BudgeteerSession.get().getTemplateFilter()));
-        add(createImportLink("importLink"));
-    }
+  public TemplatesPage() {
+    TemplatesTable table =
+        new TemplatesTable(
+            "templateTable", new TemplateListModel(BudgeteerSession.get().getTemplateFilter()));
+    add(table);
+    add(new TemplateFilterPanel("filter", Model.of(BudgeteerSession.get().getTemplateFilter())));
+    add(createImportLink());
+  }
 
-    @Override
-    protected BreadcrumbsModel getBreadcrumbsModel() {
-        return new BreadcrumbsModel(DashboardPage.class, TemplatesPage.class);
-    }
+  @Override
+  protected BreadcrumbsModel getBreadcrumbsModel() {
+    return new BreadcrumbsModel(DashboardPage.class, TemplatesPage.class);
+  }
 
-    private Link createImportLink(String id) {
-        final ImportTemplatesPage importPage = new ImportTemplatesPage(TemplatesPage.class, getPageParameters());
-        return new Link<Void>(id) {
-            @Override
-            public void onClick() {
-                setResponsePage(importPage);
-            }
-        };
-    }
+  private Link<Void> createImportLink() {
+    final ImportTemplatesPage importPage =
+        new ImportTemplatesPage(TemplatesPage.class, getPageParameters());
+    return new Link<>("importLink") {
+      @Override
+      public void onClick() {
+        setResponsePage(importPage);
+      }
+    };
+  }
 }
