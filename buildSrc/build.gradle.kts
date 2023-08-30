bc8349fbf5168852b85a2e2b0cfcc6bce217ae86
Florian Czarnecki <florian.czarnecki@adesso.de>
2023-08-30 14:35:17 +0200

    Upgrade to gradle 8.3

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.15")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.2")
-    implementation("io.freefair.gradle:lombok-plugin:6.0.0-m2")
+    implementation("io.freefair.gradle:lombok-plugin:8.3")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.20.0")
 }
